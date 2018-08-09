aa28dfc5fe13c5decc727169f0eed581884df714
suhas-kale <suhas.kale809@gmail.com>
2018-08-09 11:52:18 +0530

    first commit

---
+++ ifElse.js
@@ -0,0 +1,8 @@
+
+
+var i = 5
+if (i < 9) {
+    console.log('number is less then 9')
+} else {
+    console.log('number is greater then 9')
+}
---
+++ switch.js
@@ -0,0 +1,19 @@
+
+var a, b, result
+var ch=2
+
+console.log('1.add \n 2.sub')
+switch (ch) {
+    case 1:
+        {
+            result = 10 + 2
+            console.log(`addition is: ${result}`)
+            break
+        }
+    case 2:
+        {
+            result = 20-5
+            console.log(`substraction is: ${result}`)
+            break
+        }
+}
