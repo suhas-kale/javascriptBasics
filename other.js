c0606098a822ec38a295ab5157c2be4ca5f64d24
suhas-kale <suhas.kale809@gmail.com>
2018-08-09 12:34:38 +0530

    commit first

---
+++ other.js
@@ -0,0 +1,44 @@
+
+//prime numbers
+
+
+var num
+    for (num = 2; num < 10; num++)
+    {
+        if ((num % 2) != 0)
+             {
+                 console.log(`The prime number is ${num}`)
+             }
+    }
+
+
+
+    //do-while loop
+
+ var i = 0
+ do {
+     console.log(`Number is : ${i}`)
+     i++
+     }
+ while (i < 5)
+
+
+//Odd Even Number
+
+
+var num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    for (i = 0; i < num.length; i++) 
+        {
+            if ((num[i] % 2) == 0) 
+                {
+                    console.log(`Even Number is ${num[i]}`)
+                }
+            else 
+                {
+                    console.log(`Odd Number is ${num[i]}`)
+                }
+        }
+
+
+
